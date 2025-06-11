07f9abfd2ca20f57e96ec91906f22bbdc2ce6238
ThOMaZMe11o <thomazmello1912@gmail.com>
2025-06-11 17:23:24 -0300

    feat: implement Header component with navigation links and social icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,23 @@
+import { Github, Linkedin, Instagram, Mail } from "lucide-react";
+import '../styles/Header.css';
+
 export default function Header() {
   return (
     <>
+        <header>
+            <div className="left">
+                <a href="">Projects</a>
+                <a href="">About</a>
+                <a href="">Hobbies</a>
+                <a href="">Contact</a>
+            </div>
+            <div className="right">
+                <a href=""><Github /></a>
+                <a href=""><Linkedin /></a>
+                <a href=""><Instagram /></a>
+                <a href=""><Mail /></a>
+            </div>
+        </header>
     </>
   );
 }
