1fe31a581e899dda4f2ff5aaaa11cc7deb183e11
ThOMaZMe11o <thomazmello1912@gmail.com>
2025-06-12 07:58:48 -0300

    feat: replace Welcome component with App in main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import Welcome from './pages/Welcome.tsx'
-import Projects from './pages/Projects.tsx'
-import Hobbies from './pages/Hobbies.tsx'
-import About from './pages/About.tsx'
-import Home from './pages/Home.tsx'
-import Contact from './pages/Contact.tsx'
-import { Route } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Welcome />
+    <App />
   </StrictMode>,
 )
