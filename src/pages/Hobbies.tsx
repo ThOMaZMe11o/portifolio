cd529085efca60cbbd58ad28226c32ad00ecf626
ThOMaZMe11o <thomazmello1912@gmail.com>
2025-06-12 09:05:06 -0300

    feat: enhance Hobbies page with new layout, icons, and content; update styles for improved aesthetics

--- src/pages/Hobbies.tsx
+++ src/pages/Hobbies.tsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header"
 import '../styles/Hobbies.css';
+import { Car, Fish, Plane, Gamepad2 } from 'lucide-react'
 
 export default function Hobbies() {
     return (
@@ -8,15 +9,28 @@ export default function Hobbies() {
             <div className="h-container">
                 <div className="h-card">
                     <h1>Interesses e hobbies</h1>
-                    <p>
-                        I have a wide range of hobbies that keep me engaged and inspired. 
-                        I love reading, especially science fiction and fantasy novels. 
-                        I also enjoy hiking and exploring nature, which helps me to relax and recharge.
-                    </p>
-                    <p>
-                        In addition, I am passionate about photography and often spend my weekends capturing the beauty of the world around me.
-                        Cooking is another hobby of mine; I love experimenting with new recipes and sharing meals with friends and family.
-                    </p>    
+                    <div className="hobbies">
+                        <div className="pesca">
+                            <Fish className="svg" />
+                            <h2>Pescaria</h2>
+                            <p>Adoro pescar, especialmente em lagos tranquilos.</p>
+                        </div>
+                        <div className="viagens">
+                            <Plane className="svg" />
+                            <h2>Viagens</h2>
+                            <p>Viajar é uma das minhas maiores paixões, sempre em busca de novas aventuras.</p>
+                        </div>
+                        <div className="games">
+                            <Gamepad2 className="svg" />
+                            <h2>Games</h2>
+                            <p>Sou um grande fã de jogos, tanto digitais quanto de tabuleiro.</p>
+                        </div>
+                        <div className="carro">
+                            <Car className="svg" />
+                            <h2>Carros clássicos</h2>
+                            <p>Sou apaixonado por carros, especialmente os clássicos.</p>
+                        </div>
+                    </div>    
                 </div>
             </div>
         </>
