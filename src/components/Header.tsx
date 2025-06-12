ad9266aac4ac9398345d5ff679443bb5e83ccc72
ThOMaZMe11o <thomazmello1912@gmail.com>
2025-06-12 08:14:35 -0300

    feat: update Header component to use Link for navigation and external links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,22 @@
 import { Github, Linkedin, Instagram, Mail } from "lucide-react";
 import '../styles/Header.css';
+import { Link } from "react-router";
 
 export default function Header() {
   return (
     <>
         <header>
             <div className="left">
-                <a href="">Projects</a>
-                <a href="">About</a>
-                <a href="">Hobbies</a>
-                <a href="">Contact</a>
+                <Link to="/projects">Projects</Link>
+                <Link to="/about">About me</Link>
+                <Link to="/hobbies">Hobbies</Link>
+                <Link to="/contact">Contact</Link>
             </div>
             <div className="right">
-                <a href="https://www.github.com/ThOMaZMe11o" className="githubIcon" target="_blank"><Github style={{width: '22px', height: '22px', marginLeft: '10px'}} /></a>
-                <a href="https://www.linkedin.com/in/thomazfeitosa" className="linkedinIcon" target="_blank"><Linkedin style={{width: '22px', height: '22px', marginLeft: '10px'}} /></a>
-                <a href="https://www.instagram.com/thomaz_mellux" className="instaIcon" target="_blank"><Instagram style={{width: '22px', height: '22px', marginLeft: '10px'}} /></a>
-                <a href="mailto:thomazmello1912@gmail.com" className="mailIcon" target="_blank"><Mail style={{width: '22px', height: '22px', marginLeft: '10px'}} /></a>
+                <Link to="https://www.github.com/ThOMaZMe11o" className="githubIcon" target="_blank"><Github style={{width: '22px', height: '22px', marginLeft: '10px'}} /></Link>
+                <Link to="https://www.linkedin.com/in/thomazfeitosa" className="linkedinIcon" target="_blank"><Linkedin style={{width: '22px', height: '22px', marginLeft: '10px'}} /></Link>
+                <Link to="https://www.instagram.com/thomaz_mellux" className="instaIcon" target="_blank"><Instagram style={{width: '22px', height: '22px', marginLeft: '10px'}} /></Link>
+                <Link to="mailto:thomazmello1912@gmail.com" className="mailIcon" target="_blank"><Mail style={{width: '22px', height: '22px', marginLeft: '10px'}} /></Link>
             </div>
         </header>
     </>
