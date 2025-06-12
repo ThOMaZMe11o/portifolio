94c2e8d7546f1e52fa6ddc7dfbe08ecd0c3561cc
ThOMaZMe11o <thomazmello1912@gmail.com>
2025-06-12 08:08:00 -0300

    feat: implement routing with react-router-dom; add pages for Home, About, Contact, Hobbies, and Projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import Header from './components/Header'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import './App.css'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Hobbies from "./pages/Hobbies";
+import Projects from "./pages/Projects";
+import Welcome from "./pages/Welcome";
 
 export default function App() {
-
   return (
-    <>
-      <Header />   
-      <h1>App page</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Welcome/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/hobbies" element={<Hobbies/>} />
+        <Route path="/projects" element={<Projects/>} />
+        <Route path="/home" element={<Home/>} />
+      </Routes>
+    </Router>
   )
 }
