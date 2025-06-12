ab7f24d7333cc2bf5c2f632b9b07cb239c00e35b
ThOMaZMe11o <thomazmello1912@gmail.com>
2025-06-11 23:48:20 -0300

    feat: implement Hobbies page with updated layout and styles; add content and CSS

--- src/pages/Hobbies.tsx
+++ src/pages/Hobbies.tsx
@@ -1,10 +1,23 @@
 import Header from "../components/Header"
+import '../styles/Hobbies.css';
 
 export default function Hobbies() {
     return (
         <>
             <Header />
-            <div className="card">
+            <div className="h-container">
+                <div className="h-card">
+                    <h1>Interesses e hobbies</h1>
+                    <p>
+                        I have a wide range of hobbies that keep me engaged and inspired. 
+                        I love reading, especially science fiction and fantasy novels. 
+                        I also enjoy hiking and exploring nature, which helps me to relax and recharge.
+                    </p>
+                    <p>
+                        In addition, I am passionate about photography and often spend my weekends capturing the beauty of the world around me.
+                        Cooking is another hobby of mine; I love experimenting with new recipes and sharing meals with friends and family.
+                    </p>    
+                </div>
             </div>
         </>
     )
