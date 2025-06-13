50e357d68402f75f4c3d32ce5e8b796dce26ee80
ThOMaZMe11o <thomazmello1912@gmail.com>
2025-06-13 19:05:32 -0300

    Add empty carousel item with image tag for future content integration

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -169,7 +169,9 @@ export default function About() {
                                     <div className="carousel-item">
                                         <img src="" alt="" />
                                     </div>
-                                    <div className="carousel-item"></div>
+                                    <div className="carousel-item">
+                                        <img src="" alt="" />
+                                    </div>
                                 </div>
                             </div>
                         </div>
